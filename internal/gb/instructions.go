98fd85853c85ee34f53c75b055d6e3b7b9b3c00d
Tobias Marquardt <tmarquardt@mailbox.org>
2022-02-13 18:54:06 +0100

    Improve some comments on instructions regarding use of immediate values.

--- internal/gb/instructions.go
+++ internal/gb/instructions.go
@@ -18,28 +18,28 @@ func LDD_HL_A(mem *Memory, reg *Registers) {
 
 // ###### 16-Bit Loads ######
 
-// LD_BC_nn loads the 16 bit value pointed by PC into BC.
+// LD_BC_nn loads a 16 bit immediate value into BC.
 func LD_BC_nn(mem *Memory, reg *Registers) {
 	reg.C = mem.Read8(reg.PC)
 	reg.B = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_DE_nn loads the 16 bit value pointed by PC into DE.
+// LD_DE_nn loads a 16 bit immediate value into DE.
 func LD_DE_nn(mem *Memory, reg *Registers) {
 	reg.E = mem.Read8(reg.PC)
 	reg.D = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_HL_nn loads the 16 bit value pointed by PC into HL.
+// LD_HL_nn loads a 16 bit immediate value into HL.
 func LD_HL_nn(mem *Memory, reg *Registers) {
 	reg.L = mem.Read8(reg.PC)
 	reg.H = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_SP_nn loads the 16 bit value pointed by PC into SP.
+// LD_SP_nn loads a 16 bit immediate value into SP.
 func LD_SP_nn(mem *Memory, reg *Registers) {
 	reg.SP = mem.Read16(reg.PC)
 	reg.PC += 2
@@ -88,7 +88,7 @@ func XOR_A_HL(mem *Memory, reg *Registers) {
 	xorA(mem.Read8(addr), reg)
 }
 
-// XOR_A_n xors A with the value pointed by PC and puts result into A.
+// XOR_A_n xors A with the 8 bit immediate value and puts result into A.
 func XOR_A_n(mem *Memory, reg *Registers) {
 	n := mem.Read8(reg.PC)
 	reg.PC += 1
