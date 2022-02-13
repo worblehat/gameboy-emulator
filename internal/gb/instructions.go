c6c2017afdfd27e40b456750be4dc21dd12ff97b
Tobias Marquardt <tmarquardt@mailbox.org>
2022-02-13 18:35:19 +0100

    Implement bit test instructions and extract duplicate code from instructions into seperate functions.

--- internal/gb/instructions.go
+++ internal/gb/instructions.go
@@ -18,28 +18,28 @@ func LDD_HL_A(mem *Memory, reg *Registers) {
 
 // ###### 16-Bit Loads ######
 
-// LD_BC_nn loads the 16 bit value at PC into BC.
+// LD_BC_nn loads the 16 bit value pointed by PC into BC.
 func LD_BC_nn(mem *Memory, reg *Registers) {
 	reg.C = mem.Read8(reg.PC)
 	reg.B = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_DE_nn loads the 16 bit value at PC into DE.
+// LD_DE_nn loads the 16 bit value pointed by PC into DE.
 func LD_DE_nn(mem *Memory, reg *Registers) {
 	reg.E = mem.Read8(reg.PC)
 	reg.D = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_HL_nn loads the 16 bit value at PC into HL.
+// LD_HL_nn loads the 16 bit value pointed by PC into HL.
 func LD_HL_nn(mem *Memory, reg *Registers) {
 	reg.L = mem.Read8(reg.PC)
 	reg.H = mem.Read8(reg.PC + 1)
 	reg.PC += 2
 }
 
-// LD_SP_nn loads the 16 bit value at PC into SP.
+// LD_SP_nn loads the 16 bit value pointed by PC into SP.
 func LD_SP_nn(mem *Memory, reg *Registers) {
 	reg.SP = mem.Read16(reg.PC)
 	reg.PC += 2
@@ -49,66 +49,395 @@ func LD_SP_nn(mem *Memory, reg *Registers) {
 
 // XOR_A_A xors A with A and puts result into A.
 func XOR_A_A(mem *Memory, reg *Registers) {
-	//lint:ignore SA4000 does not make much sense but this is what the instruction does
-	reg.A = reg.A ^ reg.A
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.A, reg)
 }
 
 // XOR_A_B xors A with B and puts result into A.
 func XOR_A_B(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.B
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.B, reg)
 }
 
 // XOR_A_C xors A with C and puts result into A.
 func XOR_A_C(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.C
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.C, reg)
 }
 
 // XOR_A_D xors A with D and puts result into A.
 func XOR_A_D(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.D
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.D, reg)
 }
 
 // XOR_A_E xors A with E and puts result into A.
 func XOR_A_E(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.E
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.E, reg)
 }
 
 // XOR_A_H xors A with H and puts result into A.
 func XOR_A_H(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.H
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.H, reg)
 }
 
 // XOR_A_L xors A with L and puts result into A.
 func XOR_A_L(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ reg.L
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+	xorA(reg.L, reg)
 }
 
-// XOR_A_HL xors A with the value pointed to by HL and puts result into A.
+// XOR_A_HL xors A with the value pointed by HL and puts result into A.
 func XOR_A_HL(mem *Memory, reg *Registers) {
 	addr := (uint16(reg.H) << 8) | uint16(reg.L)
-	reg.A = reg.A ^ mem.Read8(addr)
+	xorA(mem.Read8(addr), reg)
+}
+
+// XOR_A_n xors A with the value pointed by PC and puts result into A.
+func XOR_A_n(mem *Memory, reg *Registers) {
+	n := mem.Read8(reg.PC)
+	reg.PC += 1
+	xorA(n, reg)
+}
+
+// ###### Single-Bit Operations ######
+
+// BIT_0_A tests bit 0 in register A.
+func BIT_0_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 0, reg)
+}
+
+// BIT_1_A tests bit 1 in register A.
+func BIT_1_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 1, reg)
+}
+
+// BIT_2_A tests bit 2 in register A.
+func BIT_2_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 2, reg)
+}
+
+// BIT_3_A tests bit 3 in register A.
+func BIT_3_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 3, reg)
+}
+
+// BIT_4_A tests bit 4 in register A.
+func BIT_4_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 4, reg)
+}
+
+// BIT_5_A tests bit 5 in register A.
+func BIT_5_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 5, reg)
+}
+
+// BIT_6_A tests bit 6 in register A.
+func BIT_6_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 6, reg)
+}
+
+// BIT_7_A tests bit 7 in register A.
+func BIT_7_A(mem *Memory, reg *Registers) {
+	testBit(reg.A, 7, reg)
+}
+
+// BIT_0_B tests bit 0 in register B.
+func BIT_0_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 0, reg)
+}
+
+// BIT_1_B tests bit 1 in register B.
+func BIT_1_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 1, reg)
+}
+
+// BIT_2_B tests bit 2 in register B.
+func BIT_2_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 2, reg)
+}
+
+// BIT_3_B tests bit 3 in register B.
+func BIT_3_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 3, reg)
+}
+
+// BIT_4_B tests bit 4 in register B.
+func BIT_4_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 4, reg)
+}
+
+// BIT_5_B tests bit 5 in register B.
+func BIT_5_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 5, reg)
+}
+
+// BIT_6_B tests bit 6 in register B.
+func BIT_6_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 6, reg)
+}
+
+// BIT_7_B tests bit 7 in register B.
+func BIT_7_B(mem *Memory, reg *Registers) {
+	testBit(reg.B, 7, reg)
+}
+
+// BIT_0_C tests bit 0 in register C.
+func BIT_0_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 0, reg)
+}
+
+// BIT_1_C tests bit 1 in register C.
+func BIT_1_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 1, reg)
+}
+
+// BIT_2_C tests bit 2 in register C.
+func BIT_2_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 2, reg)
+}
+
+// BIT_3_C tests bit 3 in register C.
+func BIT_3_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 3, reg)
+}
+
+// BIT_4_C tests bit 4 in register C.
+func BIT_4_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 4, reg)
+}
+
+// BIT_5_C tests bit 5 in register C.
+func BIT_5_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 5, reg)
+}
+
+// BIT_6_C tests bit 6 in register C.
+func BIT_6_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 6, reg)
+}
+
+// BIT_7_C tests bit 7 in register C.
+func BIT_7_C(mem *Memory, reg *Registers) {
+	testBit(reg.C, 7, reg)
+}
+
+// BIT_0_D tests bit 0 in register D.
+func BIT_0_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 0, reg)
+}
+
+// BIT_1_D tests bit 1 in register D.
+func BIT_1_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 1, reg)
+}
+
+// BIT_2_D tests bit 2 in register D.
+func BIT_2_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 2, reg)
+}
+
+// BIT_3_D tests bit 3 in register D.
+func BIT_3_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 3, reg)
+}
+
+// BIT_4_D tests bit 4 in register D.
+func BIT_4_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 4, reg)
+}
+
+// BIT_5_D tests bit 5 in register D.
+func BIT_5_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 5, reg)
+}
+
+// BIT_6_D tests bit 6 in register D.
+func BIT_6_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 6, reg)
+}
+
+// BIT_7_D tests bit 7 in register D.
+func BIT_7_D(mem *Memory, reg *Registers) {
+	testBit(reg.D, 7, reg)
+}
+
+// BIT_0_E tests bit 0 in register E.
+func BIT_0_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 0, reg)
+}
+
+// BIT_1_E tests bit 1 in register E.
+func BIT_1_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 1, reg)
+}
+
+// BIT_2_E tests bit 2 in register E.
+func BIT_2_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 2, reg)
+}
+
+// BIT_3_E tests bit 3 in register E.
+func BIT_3_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 3, reg)
+}
+
+// BIT_4_E tests bit 4 in register E.
+func BIT_4_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 4, reg)
+}
+
+// BIT_5_E tests bit 5 in register E.
+func BIT_5_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 5, reg)
+}
+
+// BIT_6_E tests bit 6 in register E.
+func BIT_6_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 6, reg)
+}
+
+// BIT_7_E tests bit 7 in register E.
+func BIT_7_E(mem *Memory, reg *Registers) {
+	testBit(reg.E, 7, reg)
+}
+
+// BIT_0_H tests bit 0 in register H.
+func BIT_0_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 0, reg)
+}
+
+// BIT_1_H tests bit 1 in register H.
+func BIT_1_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 1, reg)
+}
+
+// BIT_2_H tests bit 2 in register H.
+func BIT_2_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 2, reg)
+}
+
+// BIT_3_H tests bit 3 in register H.
+func BIT_3_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 3, reg)
+}
+
+// BIT_4_H tests bit 4 in register H.
+func BIT_4_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 4, reg)
+}
+
+// BIT_5_H tests bit 5 in register H.
+func BIT_5_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 5, reg)
+}
+
+// BIT_6_H tests bit 6 in register H.
+func BIT_6_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 6, reg)
+}
+
+// BIT_7_H tests bit 7 in register H.
+func BIT_7_H(mem *Memory, reg *Registers) {
+	testBit(reg.H, 7, reg)
+}
+
+// BIT_0_L tests bit 0 in register L.
+func BIT_0_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 0, reg)
+}
+
+// BIT_1_L tests bit 1 in register L.
+func BIT_1_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 1, reg)
+}
+
+// BIT_2_L tests bit 2 in register L.
+func BIT_2_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 2, reg)
+}
+
+// BIT_3_L tests bit 3 in register L.
+func BIT_3_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 3, reg)
+}
+
+// BIT_4_L tests bit 4 in register L.
+func BIT_4_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 4, reg)
+}
+
+// BIT_5_L tests bit 5 in register L.
+func BIT_5_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 5, reg)
+}
+
+// BIT_6_L tests bit 6 in register L.
+func BIT_6_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 6, reg)
+}
+
+// BIT_7_L tests bit 7 in register L.
+func BIT_7_L(mem *Memory, reg *Registers) {
+	testBit(reg.L, 7, reg)
+}
+
+// BIT_0_HL tests bit 0 of the value pointed by HL.
+func BIT_0_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 0, reg)
+}
+
+// BIT_1_HL tests bit 1 of the value pointed by HL.
+func BIT_1_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 1, reg)
+}
+
+// BIT_2_HL tests bit 2 of the value pointed by HL.
+func BIT_2_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 2, reg)
+}
+
+// BIT_3_HL tests bit 3 of the value pointed by HL.
+func BIT_3_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 3, reg)
+}
+
+// BIT_4_HL tests bit 4 of the value pointed by HL.
+func BIT_4_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 4, reg)
+}
+
+// BIT_5_HL tests bit 5 of the value pointed by HL.
+func BIT_5_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 5, reg)
+}
+
+// BIT_6_HL tests bit 6 of the value pointed by HL.
+func BIT_6_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 6, reg)
+}
+
+// BIT_7_HL tests bit 7 of the value pointed by HL.
+func BIT_7_HL(mem *Memory, reg *Registers) {
+	addr := (uint16(reg.H) << 8) | uint16(reg.L)
+	testBit(mem.Read8(addr), 7, reg)
+}
+
+// ###### Common functions used by the instructions ######
+
+// xorA xors register A with the given value and puts the result into A.
+func xorA(value uint8, reg *Registers) {
+	reg.A = reg.A ^ value
 	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
 	reg.SetFlags(zeroFlag, reg.A == 0)
 }
 
-// XOR_A_n xors A with the value pointed to by PC and puts result into A.
-func XOR_A_n(mem *Memory, reg *Registers) {
-	reg.A = reg.A ^ mem.Read8(reg.PC)
-	reg.PC += 1
-	reg.SetFlags(subtractFlag|halfCarryFlag|carryFlag, false)
-	reg.SetFlags(zeroFlag, reg.A == 0)
+// testBit checks if the given bit is set in the given value.
+func testBit(value uint8, bit uint8, reg *Registers) {
+	isSet := (value & (1 << bit)) != 0
+	reg.SetFlags(zeroFlag, !isSet)
+	reg.SetFlags(halfCarryFlag, true)
+	reg.SetFlags(subtractFlag, false)
 }
